13799e3713f71795c0cb86ec0885bafb40e8c74a
derk136 <=greepun68@gmail.com>
2020-10-14 10:49:56 +0300

    first commit 14.10.2020

--- src/App.js
+++ src/App.js
@@ -34,6 +34,33 @@ function App() {
            <div className="col-9">
               <Route path ="/profile" render={()=>{return <Profile/>}}/>
               <Route path ="/settings" render={()=>{return <settings/>}}/>
+               <table className="table">
+                   <thead>
+                   <tr>
+                       <th scope="col">ID</th>
+                       <th scope="col">Имя Фамилия</th>
+                       <th scope="col">Email</th>
+                   </tr>
+                   </thead>
+                   <tbody>
+                   <tr>
+                       <th scope="row">1</th>
+                       <td>Виктор Иванов</td>
+                       <td>ivanov@mail.com</td>
+                   </tr>
+                   <tr>
+                       <th scope="row">2</th>
+                       <td>Иван Иванов</td>
+                       <td>ivanov@mail.ru</td>
+                   </tr>
+                   <tr>
+                       <th scope="row">3</th>
+                       <td>Виктор Сергеев</td>
+                       <td>sergeev@mail.com</td>
+                       <td></td>
+                   </tr>
+                   </tbody>
+               </table>
               <Route path ="/users" render={()=>{return <users/>}}/>
                <table className="table">
                    <thead>
