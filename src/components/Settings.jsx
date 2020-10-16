83443ee3f8b842524da7d186b7259e0a793496cd
grisha136 <71751494+grisha136@users.noreply.github.com>
2020-10-16 11:59:33 +0300

    16.10.2020

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-export const Settings = ()=> {
-    return <div className="row">
-        <div className="col-2">
-            <p>Имя: Виктор</p>
-            <p>Фамилия: Иванов</p>
-            <p>Email: ivanov@mail.com</p>
-            <p>ID: 1</p>
+export class Settings extends React.Component{
+    render() {
+        return <div className="row">
+            <div className="col-2">
+                <p>Имя: Виктор</p>
+                <p>Фамилия: Иванов</p>
+                <p>Email: ivanov@mail.com</p>
+                <p>ID: 1</p>
+            </div>
         </div>
-    </div>
+    }
 }
